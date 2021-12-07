ca76724490b38f0d14e5ff8b100316a88134eb82
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2021-12-07 23:16:38 +0300

    day 7

---
+++ day7.kt
@@ -0,0 +1,16 @@
+import java.math.*
+import kotlin.math.*
+
+fun main() {
+    val data = readAllLines().single().let { it.splitDigits().map { it.toInt() } }
+    println(easy(data))
+    println(hard(data))
+}
+
+
+private fun easy(data: List<Int>) = (data.minOrNull()!!..data.maxOrNull()!!).minOf { x ->
+   data.sumOf { (it - x).absoluteValue }
+}
+private fun hard(data: List<Int>) = (data.minOrNull()!!..data.maxOrNull()!!).minOf { x ->
+    data.sumOf { (it - x).absoluteValue.let { it * (it + 1)/ 2 } }
+}
