e21c95fb380bd227714725af283389e8d9bdf2ea
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2021-12-09 10:31:52 +0300

    day 9

--- day8.kt
+++ day8.kt
@@ -6,24 +6,24 @@ fun main() {
 
 private fun easy(data: List<List<String>>) = data.sumOf { it.takeLast(4).count { it.length in listOf(2, 3, 4, 7) } }
 
-val D = listOf(
+private val D = listOf(
     "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"
 )
 
-fun genPermutations(length:Int) : Sequence<List<Int>> = when (length) {
+private fun genPermutations(length:Int) : Sequence<List<Int>> = when (length) {
     1 -> sequenceOf(listOf(0))
     else -> genPermutations(length - 1).flatMap { old ->
         (0 until length).asSequence().map { old.take(it) + listOf(length - 1) + old.drop(it) }
     }
 }
 
-fun genAll() = genPermutations(7).map { s ->
+private fun genAll() = genPermutations(7).map { s ->
     s.indices.associate { (it + 'a'.code).toChar() to (s[it] + 'a'.code).toChar() }
 }
 
-fun String.remap(code: Map<Char, Char>) = map { code[it]!! }.sortedBy { it.code }.joinToString("")
+private fun String.remap(code: Map<Char, Char>) = map { code[it]!! }.sortedBy { it.code }.joinToString("")
 
-fun decode(vals: List<String>) = genAll().single { code ->
+private fun decode(vals: List<String>) = genAll().single { code ->
     vals.all { it.remap(code) in D  }
 }
 
---
+++ day9.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val data = readAllLines()
+    println(easy(data))
+    println(hard(data))
+}
+
+private val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun lowPoints(data: List<String>) = buildList {
+    data.indices.forEach { x ->
+        data[x].indices.forEach { y ->
+            (x to y).takeIf {
+                directions.all { (dx, dy) ->
+                    data[x][y] < (data.getOrNull(x + dx)?.getOrNull(y + dy) ?: Char.MAX_VALUE)
+                }
+            }?.apply {
+                add(this)
+            }
+        }
+    }
+}
+
+private fun easy(data: List<String>) = lowPoints(data).sumOf { data[it.first][it.second].code - '0'.code + 1 }
+
+private fun dfs(x:Int, y:Int, mark:Pair<Int, Int>, used:Array<Array<Pair<Int, Int>?>>, data:List<String>) : Int = when {
+    x !in data.indices -> 0
+    y !in data[0].indices -> 0
+    used[x][y] == mark -> 0
+    data[x][y] == '9' -> 0
+    used[x][y] != null -> throw AssertionError()
+    else -> {
+        used[x][y] = mark
+        directions.sumOf { (dx, dy) -> dfs(x + dx, y + dy, mark, used, data) } + 1
+    }
+}
+
+private fun hard(data: List<String>) = Array(data.size) { Array<Pair<Int, Int>?>(data[0].length) { null }}.let { used ->
+    lowPoints(data).map { dfs(it.first, it.second, it, used, data) }.sortedDescending().take(3).reduce(Int::times)
+}
